57b583ea9810f0f729e04c30960b2d1e01c193dd
Sitaram Poudel <spoudel646@gmail.com>
2021-03-24 12:44:24 +0545

    your commit message

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom"
-import Login from './components/Login';
-import EditorPage from './components/EditorPage';
-import WriterPage from './components/WriterPage';
-
+import Login from './Screens/Login';
+import EditorPage from './Screens/EditorPage';
+import WriterPage from './Screens/WriterPage';
+import EditorTabContain from './Components/EditorTabContain'
 
 function App() {
   return (
@@ -15,7 +15,9 @@ function App() {
       <Route exact path='/' component={Login} />
       <Route exact path='/EditorPage012' component={EditorPage} />
       <Route  exact path='/WriterPage013' component={WriterPage} />
-      </Switch>
+        </Switch>
+        
+      
 
       </BrowserRouter>
   );
